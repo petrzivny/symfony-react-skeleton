7f4dc972970cf950bc9c489389e7dd5e8401c3dd
Petr Zivny <petr.zivny@workboard.com>
2023-06-01 10:57:48 +0200

    Make twig really used only in dev and test app modes

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -2,6 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
