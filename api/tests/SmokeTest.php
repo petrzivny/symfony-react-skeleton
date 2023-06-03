32e133393ed5fafe073d770012fa204ba8aa4d6e
Petr Zivny <petr.zivny@workboard.com>
2023-06-03 10:40:23 +0200

    composer require --dev symfony/test-pack

---
+++ api/tests/SmokeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use Generator;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class SmokeTest extends WebTestCase
+{
+    /** @dataProvider urlProvider */
+    public function testPageIsSuccessful(string $url): void
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        self::assertResponseIsSuccessful();
+    }
+
+    public function urlProvider(): Generator
+    {
+        yield ['/health'];
+        // ...
+    }
+}
---
+++ api/tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
+    include dirname(__DIR__) . '/config/bootstrap.php';
+} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+}
+
+// phpcs:ignore SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+}
