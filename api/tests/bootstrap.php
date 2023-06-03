73e0a565176eb7ee57274351307654f317ab6cbe
Petr Zivny <petr.zivny@workboard.com>
2023-06-03 11:38:44 +0200

    composer require --dev vimeo/psalm
    composer require --dev psalm/plugin-symfony && vendor/bin/psalm-plugin enable psalm/plugin-symfony
    
    composer require --dev weirdan/doctrine-psalm-plugin && vendor/bin/psalm-plugin enable weirdan/doctrine-psalm-plugin
    
    composer require --dev psalm/plugin-phpunit && vendor/bin/psalm-plugin enable psalm/plugin-phpunit

--- api/tests/bootstrap.php
+++ api/tests/bootstrap.php
@@ -13,6 +13,6 @@ if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
 }
 
 // phpcs:ignore SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable
-if ($_SERVER['APP_DEBUG']) {
+if (array_key_exists('APP_DEBUG', $_SERVER) && $_SERVER['APP_DEBUG']) {
     umask(0000);
 }
--- api/tests/object-manager.php
+++ api/tests/object-manager.php
@@ -10,7 +10,23 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 
 // phpcs:ignore SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+if (!array_key_exists('APP_ENV', $_SERVER)) {
+    throw new RuntimeException('APP_ENV env variable is not defined');
+}
+
+// phpcs:ignore SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable
+$appEnv = $_SERVER['APP_ENV'];
+
+// phpcs:ignore SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable
+if (!array_key_exists('APP_DEBUG', $_SERVER)) {
+    throw new RuntimeException('APP_DEBUG env variable is not defined');
+}
+
+// phpcs:ignore SlevomatCodingStandard.Variables.DisallowSuperGlobalVariable
+$appDebug = $_SERVER['APP_DEBUG'];
+
+
+$kernel = new Kernel($appEnv, (bool) $appDebug);
 $kernel->boot();
 
 $doctrine = $kernel->getContainer()->get('doctrine');
