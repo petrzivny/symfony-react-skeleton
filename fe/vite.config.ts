5c9815b1fc7684bea041f83cf848a2ad9511344e
Petr Živný <petr.zivny@gmail.com>
2023-06-30 10:03:30 +0200

    Add helm CI tests

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  cacheDir: 'node_modules/.vite',
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:81/",
+        changeOrigin: true,
+      },
+    },
+  },
 })
