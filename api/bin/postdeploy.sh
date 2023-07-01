ae16f5cfe452fe6e03f6fd9816ec78f44ec89e78
Petr Živný <petr.zivny@gmail.com>
2023-07-01 09:49:03 +0200

    Debug 13

--- api/bin/postdeploy.sh
+++ api/bin/postdeploy.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
+
+# If you don't want fetch secrets from external GCP vault (e.g. in CI pipeline) use flag "./postdeploy.sh --skip-vault-fetch"
+
 set -e
 GCP_SECRET_MANAGER_PROJECT_ID="basic-388908"
 #HTTPD_USER=$(ps axo -o user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1)
 HTTPD_USER=www-data
 CONSOLE_USER=$(whoami)
+
+options=$(getopt -o s -l skip-vault-fetch -n 'postdeploy' -- "$@")
+eval set -- "$options"
+
+skipVaultFetch=false
+if [ "$1" = "--skip-vault-fetch" ] || [ "$1" = "-s" ]
+ then
+   skipVaultFetch=true
+fi
+
 
 chmod 700 -R .
 mkdir -p var
@@ -11,8 +24,10 @@ setfacl -dR -m u:"$HTTPD_USER":rwX -m u:"$CONSOLE_USER":rwX var
 setfacl -R -m u:"$HTTPD_USER":rwX -m u:"$CONSOLE_USER":rwX var
 setfacl -R -m u:"$HTTPD_USER":rx -m u:"$CONSOLE_USER":rx .
 
-# Comment next line if you don't want to use GCP Secret Manager
-#bin/console secrets:external:decrypt-to-file $GCP_SECRET_MANAGER_PROJECT_ID
+if ! $skipVaultFetch
+  then bin/console secrets:external:decrypt-to-file $GCP_SECRET_MANAGER_PROJECT_ID
+fi
+
 COMPOSER_ALLOW_SUPERUSER=1 composer dump-autoload --no-dev --classmap-authoritative
 COMPOSER_ALLOW_SUPERUSER=1 composer dump-env prod
 bin/console cache:warmup
