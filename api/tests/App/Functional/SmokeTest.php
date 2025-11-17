81c2a8b6635d56d30b6f15bd76dd2787a602fecc
Petr Živný <petr.zivny@gmail.com>
2025-11-17 11:34:06 +0100

    Upgrade PHP 8.2->8.4

--- api/tests/App/Functional/SmokeTest.php
+++ api/tests/App/Functional/SmokeTest.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Tests\App\Functional;
 
 use Generator;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class SmokeTest extends WebTestCase
 {
-    /** @dataProvider urlProvider */
+    #[DataProvider('urlProvider')]
     public function testPageIsSuccessful(string $url): void
     {
         $client = self::createClient();
